734c62ac3d893871545ca587b0418956367991d9
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-16 15:58:36 +0530

    feat: added more parameters in user model

--- express-session-auth/models/User.js
+++ express-session-auth/models/User.js
@@ -2,11 +2,19 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
 const UserSchema = new Schema({
-  username: String,
-  hash: String,
+  username: {
+    type: String,
+    trim: true,
+    require: [true, "please enter username"],
+    max: [20, "username should be under 20 characters"],
+    lowercase: true,
+    unique: true,
+  },
+  hash: {
+    type: String,
+    require: true,
+  },
   salt: String,
 });
 
-const User = mongoose.model("User", UserSchema);
-
-module.exports = User;
+exports.User = mongoose.model("User", UserSchema);
