bd58a896ec08d6003186d8312516bbae59c5af5a
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-17 22:39:33 +0530

    feat: adding is authentication function to auth middleware

--- express-session-auth/routes/authMiddleware.js
+++ express-session-auth/routes/authMiddleware.js
@@ -1,8 +1,12 @@
 module.exports.isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
+  } else {
+    res
+      .status(401)
+      .json({ msg: "You are not authorized to view this resource" });
   }
-  res.redirect("/login");
+  // res.redirect("/login");
 };
 
 module.exports.isAdmin = (req, res, next) => {
