d3d2b2ed34583a3c667057e15dfd1ea802026f5e
kunaldhongade <kunaldhongade1@gmail.com>
2024-03-22 20:09:41 +0530

    Fix: MINOR

--- express-session-auth/app.js
+++ express-session-auth/app.js
@@ -58,18 +58,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize()); // it rerun passport every route
 app.use(passport.session()); // initialize session and if session exist use this session
 
-app.use((req, res, next) => {
-  console.log("---------------------------------");
-  console.log("req.session", req.session);
-  console.log("req.user", req.user);
-  next();
-});
 /**
  * -------------- ROUTES ----------------
  */
 
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
+
+app.use((req, res, next) => {
+  console.log("---------------hel------------------");
+  console.log("req.session", req.session);
+  console.log("req.user", req.user);
+  // next();
+});
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>404! Sorry can't find that!</h1>");
